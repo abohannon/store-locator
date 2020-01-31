84b81bbe475168231d7402a80c6ea61654984b66
Adam Bohannon <abo46n2@gmail.com>
2020-01-30 20:22:07 -0800

    Updated constants

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const MAPBOX_PUBLIC_ACCESS_TOKEN =
-  'pk.eyJ1IjoiYWJvaGFubm9uIiwiYSI6ImNrNWVkMmkzcjI1dzYzZW4wM2ZqdzkwbDIifQ.Rw64L1g9NgOoqW1FwJtslA'
+  'pk.eyJ1Ijoic3RpbGxub3Rhc2tpbmdmb3JpdCIsImEiOiJjazVrNjQ0OGcwYXRoM2VvNjljdXplY29oIn0.osX0C8NqyvuDUIMCCekxyw'
 
 export const MAPBOX_STYLE_URL =
-  'mapbox://styles/abohannon/ck5vph06n0k2g1isz9x4mmiw5'
+  'mapbox://styles/stillnotaskingforit/ck61lf3e5089u1immtt8zj0jr'
